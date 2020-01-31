a67e12e855effb163b4bd8ff67101bb627bdd4b1
Salman butt <salman0butt@gmail.com>
2020-01-31 14:50:57 +0500

    feedback tochanges

--- resources/views/common/partials/property/property_for_rent_form.blade.php
+++ resources/views/common/partials/property/property_for_rent_form.blade.php
@@ -395,8 +395,8 @@
         <div class="form-group ">
             <h3 class="u-t5">Publisert</h3>
             <label class="mb-2 form-check-label" for="published-on">
-                <input id="published-on" name="published_on" type="checkbox" class="pub_validate">Ikke vis profilbilde og lenke til
-                profilsiden før kjøperen tar kontakt med meg.
+                <input id="published-on" name="published_on" type="checkbox" class="pub_validate">Ikke vis profilbilde og
+ lenke til profilsiden
             </label><br>
             <span class="error-span published_on"></span>
         </div>
