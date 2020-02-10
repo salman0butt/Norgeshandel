184e05c92cd1c4b6ce290c75f093735ef4950bf6
hamza54158 <ameer.h@en.webicosoft.com>
2020-02-10 17:03:09 +0500

    create a landing page carousel slider and include it on landing page

--- resources/views/common/partials/property/flat_wishes_rented_description.blade.php
+++ resources/views/common/partials/property/flat_wishes_rented_description.blade.php
@@ -61,52 +61,8 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        @if(is_countable($property_data->media) > 0)
-                            @foreach($property_data->media as $key=>$val)
-                                <li data-target="#carouselExampleIndicators" data-slide-to="{{$key}}" @if($key == 0) class="active" @endif></li>
-                            @endforeach
-                        @else
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        @endif
-
-                    </ol>
-                    <div class="carousel-inner">
-
-                        <?php $i = 0; ?>
-                        @if(is_countable($property_data->media) > 0)
-                            @foreach($property_data->media as $key=>$val)
-                                <?php
-                                $unique_name  =  $val->name_unique;
-                                $path  =    \App\Helpers\common::getMediaPath($property_data);
-                                $full_path  = $path."". $unique_name;
-                                ?>
-
-                                <div class="carousel-item <?php echo($i == 0 ? "active" : ""); ?>">
-                                    <img class="d-block w-100" src="{{ $full_path }}" alt="First slide">
-                                </div>
-
-                                <?php $i++ ?>
-                            @endforeach
-                        @else
-                            <div class="carousel-item <?php echo($i == 0 ? "active" : ""); ?>">
-                                <img class="d-block w-100" src="{{ asset('/public/uploads/banners/1280x720.png') }}"
-                                     alt="First slide">
-                            </div>
-                        @endif
-
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                {{--<img src="{{$full_path}}" alt="" class="img-fluid">--}}
+                @php $name = $property_data->media; @endphp
+                @include('user-panel.partials.landing_page_slider',compact('name'));
             </div>
             <div class="col-md-12 text-center">
                 <div class="single-realestate-caption" style="width:50%;margin:auto;margin-top: -20px;">{{$property_data->headline ? $property_data->headline : 'N/A'}}</div>
