6599c097ba11d6b1733f04deda6d64f070d29449
Salman butt <salman0butt@gmail.com>
2020-02-14 16:25:08 +0500

    worked property edit update and delete

--- resources/views/user-panel/property/flat_wishes_rented.blade.php
+++ resources/views/user-panel/property/flat_wishes_rented.blade.php
@@ -11,7 +11,12 @@
 
             <div class="row">
                 <div class="col-md-10 offset-md-1">
-                    @include('common.partials.property.flat_wishes_rented_form')
+                @if(Request::is('new/flat/wishes/rented/*/edit'))
+                     @include('common.partials.property.edit_flat_wishes_rented_form')
+                @else
+                      @include('common.partials.property.flat_wishes_rented_form')
+                @endif
+                   
                 </div>
             </div>
         </div>
@@ -34,13 +39,14 @@
 
             e.preventDefault();
 
-            var url = '{{url('add/flat/wishes/rented')}}';
             $('.notice').html("");
             var myform = document.getElementById("flat_wishes_rented_form");
             var fd = new FormData(myform);
             e.preventDefault();
             var l = Ladda.create(this);
             l.start();
+            @if(Request::is('new/flat/wishes/rented/*/edit'))
+             var url = "{{url('new/flat/wishes/rented/'.$flat_wishes_rented->id)}}";
             $.ajax({
                 type: "POST",
                 url: url,
@@ -66,6 +72,35 @@
             return false;
 
     });
+    @else
+      var url = '{{url('new/flat/wishes/rented')}}';
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: fd,
+                dataType: "json",
+                processData: false,
+                contentType: false,
+                success: function(data){
+                        $('.notice').append('<div class="alert alert-success">Eiendom lagt til!</div>');
+                },
+                error: function(jqXhr, json, errorThrown){// this are default for ajax errors
+                        var errors = jqXhr.responseJSON;
+                        console.log(errors.errors);
+                        var html="<ul>";
+                        $.each( errors.errors, function( index, value ){
+                        console.log(value);
+                        html += "<li>"+value+"</li>";
+                        });
+                        html += "</ul>";
+                        $('.notice').append('<div class="alert alert-danger">'+html+'</div>');
+                    },
+            }).always(function() { l.stop(); });
+            return false;
+
+    });
+
+    @endif
 
     </script>
 
