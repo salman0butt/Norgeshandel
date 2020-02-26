33806f42ae812137ff35a4b577f896bd5004eae3
unknown <ameer@digitalmx.no>
2020-02-26 16:53:46 +0500

    send email after changing password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class ResetPasswordController extends Controller
 {
@@ -34,6 +36,17 @@ class ResetPasswordController extends Controller
      */
     public function __construct()
     {
+        if(Auth::user()){
+            $date = date('d-m-Y G:i:s');
+            $to_name = Auth::user()->username;
+            $to_email = Auth::user()->email;
+            Mail::send('mail.changed_password',compact('date'), function ($message) use ($to_name, $to_email) {
+                $message->to($to_email, $to_name)->subject('Passord endret');
+                $message->from('developer@digitalmx.no', 'NorgesHandel ');
+            });
+        }
+
+
         $this->middleware('guest');
     }
 }
