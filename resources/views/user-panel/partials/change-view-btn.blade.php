11b4f42cad67d656f3841a4abf65b71b87255898
unknown <ameer@digitalmx.no>
2020-03-24 10:13:16 +0500

    chnage the view of search properties from global search

--- resources/views/user-panel/partials/change-view-btn.blade.php
+++ resources/views/user-panel/partials/change-view-btn.blade.php
@@ -1,9 +1,3 @@
-@php
-    $search = '';
-    if(Request::get('search')){
-        //$search = 'search='.Request::get('search');
-    }
-@endphp
 @if($col == 'grid')
     <a href="?view=list" data-view="list" class="dme-btn-rounded-back-only" id="view">
         <i class="fa fa-list"></i>
