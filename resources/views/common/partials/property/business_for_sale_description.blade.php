3e38929bbec758611d176aa9caa34a3cc3c442b3
unknown <ameer@digitalmx.no>
2020-04-06 18:04:38 +0500

    hide static information from all property pages

--- resources/views/common/partials/property/business_for_sale_description.blade.php
+++ resources/views/common/partials/property/business_for_sale_description.blade.php
@@ -149,8 +149,8 @@
                             <a href="{{url('property/business-for-sale/search?user_id='.$property_data->ad->user->id)}}">Flere annonser fra annonsør</a>
                         </div>
                     @endif
-                    <div class="mb-2"><a href="https://www.dnbeiendom.no/Autoprospekt/302190059" target="_blank" rel="noopener external" data-controller="trackCustomerLink">Bestill komplett, utskriftsvennlig
-                            salgsoppgave</a></div>
+                    {{--<div class="mb-2"><a href="https://www.dnbeiendom.no/Autoprospekt/302190059" target="_blank" rel="noopener external" data-controller="trackCustomerLink">Bestill komplett, utskriftsvennlig--}}
+                            {{--salgsoppgave</a></div>--}}
                     @if(!$property_data->ad->is_mine())
                         <div class="mb-2"><a href="{{url('messages/new', $property_data->ad->id)}}">Send melding</a></div>
                     @endif
--- resources/views/common/partials/property/commercialproperty_for_rent_description.blade.php
+++ resources/views/common/partials/property/commercialproperty_for_rent_description.blade.php
@@ -242,8 +242,8 @@
                             <a href="{{url('property/commercial-property-for-rent/search?user_id='.$property_data->ad->user->id)}}">Flere annonser fra annonsør</a>
                         </div>
                     @endif
-                    <div class="mb-2"><a href="https://www.dnbeiendom.no/Autoprospekt/302190059" target="_blank" rel="noopener external" data-controller="trackCustomerLink">Bestill komplett, utskriftsvennlig
-                            salgsoppgave</a></div>
+                    {{--<div class="mb-2"><a href="https://www.dnbeiendom.no/Autoprospekt/302190059" target="_blank" rel="noopener external" data-controller="trackCustomerLink">Bestill komplett, utskriftsvennlig--}}
+                            {{--salgsoppgave</a></div>--}}
                     @if(!$property_data->ad->is_mine())
                         <div class="mb-2"><a href="{{url('messages/new', $property_data->ad->id)}}">Send melding</a></div>
                     @endif
